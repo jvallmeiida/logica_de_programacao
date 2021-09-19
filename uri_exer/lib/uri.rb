5e01d1c8b4d14e29be2677e603a2fd7d57efa28f
JV_ALMEIDA <aalmeidadev@gmail.com>
2021-09-19 14:48:33 -0300

    refatoring to exercise

--- uri_exer/lib/uri.rb
+++ uri_exer/lib/uri.rb
@@ -1,15 +1,26 @@
 class Uri
-  def calcular_compras(code_product, units_product, prices_product)
-    value = []
-    units_product.each_with_index do |unit_product, index_unit|
-      prices_product.each_with_index do |price_product, index_price|
-        if index_unit == index_price
-          value << unit_product * price_product
-        end
-      end
-    end
-    total = value.reduce { |sum, num| sum + num }
+  attr_reader :code_product, :units_product, :prices_product
 
-    "VALOR A PAGAR: R$ #{'%.2f' % total}"
+  # To initialize the properties of a class we can use the initialize method
+  # which is called before any method.
+  def initialize(code_product:, units_product:, prices_product:)
+    @code_product = code_product
+    @units_product = units_product
+    @prices_product = prices_product
+  end
+
+  def calcular_compras
+    # zip method creates an array of arrays according to its index, example:
+    # a = [1, 2, 3]
+    # b = [4, 5, 6]
+    # a.zip(b) = [[1, 4], [2, 5], [3, 6]]
+    units_product.zip(prices_product)
+    # map generates a new array by multiplying each array of arrays
+    # and generating the values of each purchased item
+                 .map { |arr| arr.reduce(:*) }
+    # reduce takes these values from purchased items and sums them back to a final result
+                 .reduce(:+)
+    # then passes the parameter q is between | q is the result of the previous expression
+                 .then { |total| "VALOR A PAGAR: R$ #{format('%.2f', total)}" }
   end
 end
