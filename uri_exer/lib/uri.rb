a99aab361139450aa71608858760365bfd175ae5
JV_ALMEIDA <aalmeidadev@gmail.com>
2021-09-19 15:20:48 -0300

    Updating translate and adjusting commits

--- uri_exer/lib/uri.rb
+++ uri_exer/lib/uri.rb
@@ -9,18 +9,18 @@ class Uri
     @prices_product = prices_product
   end
 
-  def calcular_compras
-    # zip method creates an array of arrays according to its index, example:
-    # a = [1, 2, 3]
-    # b = [4, 5, 6]
-    # a.zip(b) = [[1, 4], [2, 5], [3, 6]]
+  # zip method creates an array of arrays according to its index, example:
+     # a = [1, 2, 3]
+     # b = [4, 5, 6]
+     # a.zip(b) = [[1, 4], [2, 5], [3, 6]]
+
+  # .map generates a new array by multiplying each array of arrays generating the values of each purchased item
+  # .reduce takes these values from purchased items and sums them back to a final result
+  # .then passes the parameter q is between | q is the result of the previous expression
+  def calculate_purchases
     units_product.zip(prices_product)
-    # map generates a new array by multiplying each array of arrays
-    # and generating the values of each purchased item
                  .map { |arr| arr.reduce(:*) }
-    # reduce takes these values from purchased items and sums them back to a final result
                  .reduce(:+)
-    # then passes the parameter q is between | q is the result of the previous expression
                  .then { |total| "VALOR A PAGAR: R$ #{format('%.2f', total)}" }
   end
 end
--- uri_exer/spec/uri_spec.rb
+++ uri_exer/spec/uri_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'Calcular' do
-  it 'calcular compras com produtos' do
+  it 'calculate purchases for products' do
     code_product = [1, 2, 3]
     units_product = [2, 4, 1]
     prices_product = [10.5, 10.0, 8.0]
 
     expect(Uri.new(code_product: code_product, units_product: units_product,
-                   prices_product: prices_product).calcular_compras).to eq("VALOR A PAGAR: R$ 69.00")
+                   prices_product: prices_product).calculate_purchases).to eq("VALOR A PAGAR: R$ 69.00")
   end
 end
